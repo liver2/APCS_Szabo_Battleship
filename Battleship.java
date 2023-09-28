7ff9432bf4020cec8d994f1965cdd99b8aced68a
Liver S. <szabooli24@7hills.org>
2023-09-28 12:39:28 -0400

    printboard??

--- Battleship.java
+++ Battleship.java
@@ -1,16 +1,7 @@
 public class Battleship {
     public static void main(String[] args) {
-        int numRows = 10;
-        int numCols = 10;
-        int[] ships = new int[5];
+        Board liverB = new Board(10,10);
 
-        int[][] game = new int[numRows][numCols];
-
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numCols; c++) {
-                System.out.print(game[r][c] + " ");
-            }
-            System.out.print("\n");
-        }
+        liverB.printBoard();
     }
 }
--- Board.java
+++ Board.java
@@ -1,3 +1,23 @@
 public class Board {
-    
+    int numRows;
+    int numCols;
+
+    public Board(int rows, int columns) {
+        numRows = rows;
+        numCols = columns;
+    }
+
+    public Board(){
+        numRows = 10;
+        numCols = 10;
+    }
+
+    public void printBoard() {
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                System.out.print(game[r][c] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
