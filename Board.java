91cab57844e75445db327f1f1c60324c48f1ef2f
Liver S. <szabooli24@7hills.org>
2023-09-28 12:42:09 -0400

    start conceptualizing battleship

--- Board.java
+++ Board.java
@@ -1,23 +1,5 @@
 public class Board {
-    int numRows;
-    int numCols;
+    int[][] grid = new int[5][5];
 
-    public Board(int rows, int columns) {
-        numRows = rows;
-        numCols = columns;
-    }
-
-    public Board(){
-        numRows = 10;
-        numCols = 10;
-    }
-
-    public void printBoard() {
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numCols; c++) {
-                System.out.print(game[r][c] + " ");
-            }
-            System.out.print("\n");
-        }
-    }
+    
 }
