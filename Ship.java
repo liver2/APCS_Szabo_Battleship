a30561bd67ae45f979e27a77c06c171cc54fc216
liver2 <124078178+liver2@users.noreply.github.com>
2023-09-29 20:52:31 -0400

    public void shot

--- Ship.java
+++ Ship.java
@@ -1,15 +1,35 @@
 public class Ship {
     int length;
-    int[][] position; // x position, y position
+    int[][] position; // x position, y position, status
+    boolean sunk = false;
 
     public Ship (int len, int posStartX, int posStartY, String orientation) {
         length = len;
-        position = new int[length][2];
+        position = new int[length][3];
         position[0][0] = posStartX;
         position[0][1] = posStartY;
-        for (int i = 1; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             position[i][0] = posStartX + i*(Boolean.compare(orientation == "WEST", true)) - i*(Boolean.compare(orientation == "EAST", true));
             position[i][1] = posStartY + i*(Boolean.compare(orientation == "SOUTH", true)) - i*(Boolean.compare(orientation == "NORTH", true));
+            position[i][2] = 1; // 1 means not sunk. 0 means sunk.
         }
     }
-}
+
+    public void shot (int x, int y) {
+        int counter = 0;
+
+        for (int k = 0; k < length; k++) {
+            if (position[k][0] == x && position[k][1] == y) {
+                position[k][2] = 0;
+            }
+        }
+
+        for (int l = 0; l < length; l++) {
+                counter += position[l][2];
+            }
+        }
+
+        if (counter == length) {
+            boolean sunk = true;
+        }
+    }
