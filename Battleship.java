5417d7ef0f08b607b042552ef9969613b85f664d
liver2 <124078178+liver2@users.noreply.github.com>
2023-10-01 17:42:18 -0400

    might have to rethink structure.. taking break

--- Battleship.java
+++ Battleship.java
@@ -4,60 +4,6 @@ public class Battleship {
     static Board pBoard = new Board(); // Player Board
     static Board cBoard = new Board(); // Computer Board
 
-    public static void promptShipPlacement(int len) {
-        Scanner scan = new Scanner(System.in);
-        int x;
-        int y;
-        String orientation;
-
-        System.out.println("Please specify an X coordinate for your ship with length " + len + ".");
-        do {
-            x = scan.nextInt();
-        } while (!(x >= 0 && x <= 10));
-
-        System.out.println("Please specify a Y coordinate for your ship with length " + len + ".");
-        do {
-            y = scan.nextInt();
-        } while (!(y >= 0 && y <= 10));
-
-        System.out.println("Please specify an orientation for your ship with length " + len + ".");
-        System.out.println("Specify north with n, south with s, east with e, and west with w.");
-        do {
-            scan.nextLine(); // To clear the new line created after scanning an int
-            orientation = scan.nextLine();
-            /* For debugging: */ System.out.println(shipPlacementCheck(x, y, len, orientation));
-        } while (!(orientation == "n" || orientation == "s" || orientation == "e" || orientation == "w") 
-                 || 
-                 !(shipPlacementCheck(x, y, len, orientation) == true));
-
-        System.out.println("Let's see your ship on the board...\n");
-
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < 1; j++) {
-                pBoard.setIndicator();
-            }
-        }
-    }
-
-    public static boolean shipPlacementCheck(int x, int y, int len, String orientation) {
-        int[][] pos = new int[len][3];
-        pos[0][0] = x;
-        pos[0][1] = y;
-        for (int i = 0; i < len; i++) {
-            pos[i][0] = x + i*(Boolean.compare(orientation == "w", true)) - i*(Boolean.compare(orientation == "e", true));
-            pos[i][1] = y + i*(Boolean.compare(orientation == "s", true)) - i*(Boolean.compare(orientation == "n", true));
-        }
-
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < 1; j++) {
-                if (pos[i][j] > 10 || pos[i][j] < 0) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
     public static void main(String[] args) {
         System.out.println("--- Battleship ---");
 
--- Ship.java
+++ Ship.java
@@ -5,7 +5,7 @@ public class Ship {
     int[][] position; // x position, y position, status
     boolean sunk = false;
 
-    public Ship (int len, int posStartX, int posStartY, String orientation) {
+    public void setShipParams (int len, int posStartX, int posStartY, String orientation) {
         length = len;
         position = new int[length][3];
         position[0][0] = posStartX;
@@ -15,6 +15,61 @@ public class Ship {
             position[i][1] = posStartY + i*(Boolean.compare(orientation == "SOUTH", true)) - i*(Boolean.compare(orientation == "NORTH", true));
             position[i][2] = 1; // 1 means not sunk. 0 means sunk.
         }
+    }
+
+    public static void promptShipPlacement(int len, Board board) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        String orientation;
+
+        System.out.println("Please specify an X coordinate for your ship with length " + len + ".");
+        do {
+            x = scan.nextInt();
+        } while (!(x >= 0 && x <= 10));
+
+        System.out.println("Please specify a Y coordinate for your ship with length " + len + ".");
+        do {
+            y = scan.nextInt();
+        } while (!(y >= 0 && y <= 10));
+
+        System.out.println("Please specify an orientation for your ship with length " + len + ".");
+        System.out.println("Specify north with n, south with s, east with e, and west with w.");
+        do {
+            scan.nextLine(); // To clear the new line created after scanning an int
+            orientation = scan.nextLine();
+            /* For debugging: */ System.out.println(shipPlacementCheck(x, y, len, orientation));
+        } while (!(orientation == "n" || orientation == "s" || orientation == "e" || orientation == "w") 
+                 || 
+                 !(shipPlacementCheck(x, y, len, orientation) == true));
+
+        System.out.println("Let's see your ship on the board...\n");
+
+        setShipParams(len, x, y, orientation);
+
+        for (int i = 0; i < len; i++) {
+            board.setIndicator(); // this works??????
+        }
+    }
+
+    public static boolean shipPlacementCheck(int x, int y, int len, String orientation) {
+        int[][] pos = new int[len][3];
+        pos[0][0] = x;
+        pos[0][1] = y;
+        for (int i = 0; i < len; i++) {
+            pos[i][0] = x + i*(Boolean.compare(orientation == "w", true)) - i*(Boolean.compare(orientation == "e", true));
+            pos[i][1] = y + i*(Boolean.compare(orientation == "s", true)) - i*(Boolean.compare(orientation == "n", true));
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < 1; j++) {
+                if (pos[i][j] > 10 || pos[i][j] < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public void shot (int x, int y) {
@@ -33,6 +88,10 @@ public class Ship {
         if (counter == length) {
             sunk = true;
         }
+    }
+
+    public int getPosition(int x, int y) {
+        return position[x][y];
     }
 
     public int getLength() {
