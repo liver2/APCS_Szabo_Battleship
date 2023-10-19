ea8b1d7765db6cfdbe82537ef2e76a81a6bddce3
liver2 <124078178+liver2@users.noreply.github.com>
2023-10-18 21:55:56 -0400

    making instructions more clear & commenting

--- Main.java
+++ Main.java
@@ -30,26 +30,25 @@ public class Main {
                 System.out.println("Welcome to Battleship!");
                 System.out.println("For this version of Battleship, you can choose 2 modes: Fast and Regular.");
                 System.out.println("Let me explain the rules.\n"); // Places new line
-                System.out.println("In Regular mode, you and a friend will each place 5 ships — whose positions the other will then try to guess — on a 10x10 board.");
-                System.out.println("Be aware that North is up and East is right!");
-                System.out.println("Also, X is horizontal and Y is vertical.");
+                System.out.println("In Regular mode, you and a friend will each place 5 ships — whose positions the other will then try to guess — on a 10x10 grid.");
                 System.out.println("The ships will be lengths 2, 3, 3, 4, and 5; this means they will occupy spaces horizontally or vertically next to each other.");
                 System.out.println("Ships cannot be placed diagonally; only horizontally and vertically. They cannot extend off the board, and they cannot overlap.");
                 System.out.println("After you've placed your ships, you and your friend will take turns guessing the positions of each others' ships.");
-                System.out.println("You will do this using coordinates.");
-                System.out.println("Hitting one part of a ship does not mean it's sunk. For example, if a ship is length 5, you need to hit all 5 of its segments to sink it.");
-                System.out.println("The indicator on your guessing board, S, will appear where the other person's ship used to be when you've sunk a ship.");
+                System.out.println("You will do this using coordinates: You will fire one shot at a single spot.");
+                System.out.println("If you hit one part of a ship, an X will show where you fired your shot.");
+                System.out.println("If you miss, there will be a lowercase M there instead.");
+                System.out.println("When you've sunk a ship, the indicator S will appear over the squares where the ship formerly was.");
                 System.out.println("If you can sink your friend's 5 ships, you win! Vice versa; whoever gets all their five ships sunk first loses.\n");
                 System.out.println("In Fast mode, a computer will randomize the placement of 3 ships on an 8x8 board.");
                 System.out.println("The ships will be lengths 2, 3, and 4.");
-                System.out.println("You will guess where the computer's ships are."); 
-                System.out.println("You will know when you hit a ship, and you will know when a complete ship has been sunk.");
-                System.out.println("When you sink all the ships, you will learn how many turns it took you; lower is better.\n");
+                System.out.println("You will guess where the computer's ships are; same rules about indicators apply.");
+                System.out.println("Remember: X is hit, S is sunk, m is miss.");
+                System.out.println("When you sink all the ships, you will learn how many turns it took you; lower is better!\n");
                 break;
             }
 
             if (s1.equals("n")) {
-                break;
+                break; // exiting loop and moving to the "select mode"
             }
         } while (!s1.equals("y") && !s1.equals("n"));
 
